e47c7711dad9f5cf28493205d1ac754e5309e0a4
M211-netizen <muhammadnabil321@gmail.com>
2025-11-19 15:56:53 +0200

    Add files via upload
    Add Testimonials & Pricing Sections + Animations

---
+++ script.js
@@ -0,0 +1,2 @@
+document.getElementById("darkToggle").onclick = () => {document.body.classList.toggle("dark");};
+const elements=document.querySelectorAll('.reveal');const observer=new IntersectionObserver((entries)=>{entries.forEach(entry=>{if(entry.isIntersecting)entry.target.classList.add('active');});},{threshold:0.2});elements.forEach(el=>observer.observe(el));
